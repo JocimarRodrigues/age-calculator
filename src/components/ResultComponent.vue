f26d6cb37e1bb0d983c87e699509968c5e33e965
John Rodrigues <jocimarrodrigues1337@gmail.com>
2023-12-19 18:39:30 -0300

    Lógica para calcular idade

--- src/components/ResultComponent.vue
+++ src/components/ResultComponent.vue
@@ -1,11 +1,12 @@
 <template>
-    <div v-if="dados">
-        <h1>{{ dados.day}}</h1>
+    <div v-if="data">
+        <h1>{{ data.day }}</h1>
+        <h1>{{ data.month }}</h1>
     </div>
 </template>
 
 <script lang=ts>
-import { defineComponent, toRefs, watch } from 'vue';
+import { defineComponent, toRefs, watch,ref } from 'vue';
 import { IFormData } from '@/interfaces/IFormData';
 export default defineComponent({
     name: 'ResultComponent',
@@ -19,25 +20,44 @@ export default defineComponent({
 
         const { formData } = toRefs(props) // toRefs para manter reatividade das props
 
-        const {day, month, year} = formData.value || {}
-
-
+        let data = ref()
 
         watch(formData, (newFormData) => {
-            // const obj = newFormData
-            // console.log(obj?.day)
-            if(newFormData) {
+
+            if (newFormData) {
+                const currentDate = new Date()
+                const birthDate = new Date(newFormData.year, newFormData.month - 1, newFormData.day); 
+                const ageInMilliseconds = currentDate.getTime() - birthDate.getTime();
+                const ageInDays = Math.floor(ageInMilliseconds / (1000 * 60 * 60 * 24));
+
+                const years = Math.floor(ageInDays / 365);
+                const months = Math.floor((ageInDays % 365) / 30.4375); 
+                const days = ageInDays % 30.4375;
 
 
-                console.log(newFormData.day)
+
+
+                const result = {
+                    day: days,
+                    month: months,
+                    year: years
+                }
+
+
+
+                data.value = result
+
             }
-    });
-        
+        });
+
+        console.log(data)
+
         // const dia = formData.value?.day
         // let data = dia
-     
+
         return {
             dados: formData,
+            data
             // data
         }
 
